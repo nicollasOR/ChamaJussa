5a341e9b4dc636ee098479b7f9b65556a94baa2e
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-05 10:57:27 -0300

    _

--- chamajussa/jussaCalls/App.tsx
+++ chamajussa/jussaCalls/App.tsx
@@ -1,14 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, ImageComponent, StyleSheet, Text, TouchableHighlight, View, TextInput } from 'react-native';
+import { Image, ImageComponent, StyleSheet, Text, TouchableHighlight, View, TextInput,   } from 'react-native';
 import { Login } from './src/pages/login/login';
 import { Perfil } from './src/pages/perfil/Perfil';
+import { Notificacoes } from './src/pages/notificacoes/Notificacoes';
+import { Criar } from './src/pages/api/criarOS/Criar';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 // import { Login } from './src/pages/login/Login';
 export default function App() {
   return (
-    // <Login/>
-    <Perfil/>
+
+    <>
+    {/* AreaProvider => calcular o tamanho das extremidades (topo e rodape) */}
+    {/* AreaView => aplica o padrao de margem necessaria para o tamanho do celular */}
+    {/* <SafeAreaProvider style={styles.safeareaview}> */}
+      {/* <SafeAreaView> */}
+        {/* <StatusBar style='light'/> */}
+        <Login/>
+      {/* </SafeAreaView> */}
+    {/* </SafeAreaProvider> */}
+    </>
   )
 }
+
+const styles = StyleSheet.create({
+  safeareaview: {
+    // flex: 1,
+    backgroundColor: `#F3F4F6`
+  }
+})
+
+// SafeAreaView
 
 
 
