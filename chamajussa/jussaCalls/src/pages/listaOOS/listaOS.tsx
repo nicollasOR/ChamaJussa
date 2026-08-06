4619b54b2ff4241da2b5ebc7c62cca3afc38f2f5
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-06 10:32:33 -0300

    :sparkles: feat: add structure listaOS :sparkles:

---
+++ chamajussa/jussaCalls/src/pages/listaOOS/listaOS.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+export const listaOS = () => {
+  return (
+<>
+<View style={styles.body}>
+<View>
+    
+</View>
+</View>
+</>
+
+)
+}
+
+
+const styles = StyleSheet.create({
+    body: {
+
+    },
+
+
+})
