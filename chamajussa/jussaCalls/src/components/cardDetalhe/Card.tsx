06f3a500425f66ac7bd6df427c46e7462f048bcf
nicollasOR <nicollas.oliveira.rufino30@gmail.com>
2026-08-06 10:27:15 -0300

    :sparkles: feat: start structure cardDetalhe :sparkles:

---
+++ chamajussa/jussaCalls/src/components/cardDetalhe/Card.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { StyleSheet, View, Image, Text } from 'react-native'
+
+type propsText = {
+    texto: string
+}
+
+export const Card = () => {
+  return (
+    // <div>Card</div>
+    <>
+    <View>
+        <Image></Image>
+    <View>
+        <Text></Text>
+        <Text></Text>
+    </View>
+    </View>
+    </>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    
+})
